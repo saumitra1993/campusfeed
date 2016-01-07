528b9fc1438825980fe82b19d5ec92d0711a0ae2
Anik Chatterjee <Anik Chatterjee>
2016-01-07 20:52:12 +0530

    s added API for adding the time table

--- service/ig/eventtime.py
+++ service/ig/eventtime.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from handlers.mail import send_email
 from google.appengine.api import blobstore
 from service._users.sessions import BaseHandler
-from db.database import Events, EventMatches, EventStandings
+from db.database import Events, EventMatches, EventStandings,Departments
 from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.api import images
 from google.appengine.ext import ndb
@@ -20,20 +20,36 @@ class EventTime(webapp2.RequestHandler):
 
 		event_code = self.request.get('event_id')
 		time_string = self.request.get('time')
-		event_type = self.request.get('event_type')
 		location = self.request.get('location')
 		match_type = self.request.get('match_type')
 		dept1_code = self.request.get('dept1')
 		dept2_code = self.request.get('dept2')
 
-		if event_type == 'tournament' and match_type == 'qf':
-			
+		result1=Departments.query(Departments.dept1_code==dept1_code).fetch()
+		result2=Departments.query(Departments.dept2_code==dept2_code).fetch()
+		result3=Events.query(Events.code==event_code).fetch()
+		if len(result1) == 1 and len(result2) == 1 and len(result3) == 1:
+			dept1_ptr = result1[0].key
+			dept2_ptr = result2[0].key
+			event_ptr = result3[0].key
+			time = string_to_date(time_string)
+			eve=EventMatches()
+			eve.event_ptr = event_ptr
+			eve.dept1_ptr = dept1_ptr
+			eve.dept2_ptr = dept2_ptr
+			eve.match_time = time
+			eve.match_type = match_type
+			eve.put()
+			self.response.set_status(200,"Awesome")
+			dict_ = {}
+			dict_['status'] = "SUCCESS"
+		else:
+			self.response.set_status(400,"Failed")
+			dict_ = {}
+			dict_['status'] = "FAILED"
 
-
-		self.response.set_status(200,"Awesome")
-		dict_ = {}
-		dict_['status'] = "SUCCESS"
 		self.response.write(json.dumps(dict_))
+
 
 
 			
