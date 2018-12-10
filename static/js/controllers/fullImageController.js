ee260bd550679f1a3314af02914c6be8b25935c3
Shivam <shivam270295@gmail.com>
2018-12-10 23:32:59 +0530

    Reset source

--- static/js/controllers/fullImageController.js
+++
@@ -1,9 +0,0 @@
-angular.module('campusfeed').controller('ModalInstanceCtrl', function ($scope, $modalInstance, selectedImgUrl) {
-
-  $scope.selectedImgUrl = selectedImgUrl;
-
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
-
-});
